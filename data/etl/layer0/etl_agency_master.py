67a724cdaf4daa578287465ec88e772b451d5b4a
suwantong rata <suwantong.rata@bcg.com>
2020-11-06 20:58:21 +0100

    formatting

--- data/etl/layer0/etl_agency_master.py
+++ data/etl/layer0/etl_agency_master.py
@@ -19,31 +19,34 @@ class EtlAgencyMaster(Etl):
 
     def _process_raw_data(self, raw_df: pd.DataFrame = None):
         df = raw_df
-        df = df.rename(columns={"adresse": "agency_address",
-                        "Code Postal": "postal_code",
-                        "Ville": "city",
-                                "agence_name": "agency_name",
-                        "agence_url_0": "agency_url",
-                                "tel_1": "telephone",
-                        "nb_avis": "num_reviews",
-                        "note": "score",
-                        "prestations": "services",
-                        "reseaux": "network",
-                        "teaser-avis": "review_example",
-                        "url_pj": "agency_url_pj",
-                        "agence_id": "image_code"
-                        })
+        df = df.rename(
+            columns={
+                "adresse": "agency_address",
+                "Code Postal": "postal_code",
+                "Ville": "city",
+                "agence_name": "agency_name",
+                "agence_url_0": "agency_url",
+                "tel_1": "telephone",
+                "nb_avis": "num_reviews",
+                "note": "score",
+                "prestations": "services",
+                "reseaux": "network",
+                "teaser-avis": "review_example",
+                "url_pj": "agency_url_pj",
+                "agence_id": "image_code",
+            }
+        )
 
         list_postal_code = df.postal_code.unique()
         for c in list_postal_code:
             # Make folder to save outputs if not existed
             if not os.path.exists(
-                    os.path.join(
-                        "/Users/jacquemart rata/Documents/04_PERSO/Immo/00_data",
-                        "02_agency_fees",
-                        "agency_master",
-                        str(c),
-                    )
+                os.path.join(
+                    "/Users/jacquemart rata/Documents/04_PERSO/Immo/00_data",
+                    "02_agency_fees",
+                    "agency_master",
+                    str(c),
+                )
             ):
                 os.makedirs(
                     os.path.join(
@@ -61,8 +64,11 @@ class EtlAgencyMaster(Etl):
                     "02_agency_fees",
                     "agency_master",
                     str(c),
-                    self._config.general["timestamp"] + "_"+
-                                                        "agency_master_" + str(c) + ".csv",
+                    self._config.general["timestamp"]
+                    + "_"
+                    + "agency_master_"
+                    + str(c)
+                    + ".csv",
                 ),
                 index=False,
             )
