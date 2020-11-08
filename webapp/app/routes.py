21deb38a72fe1f24209721a762e354eca752cf9a
suwantong rata <suwantong.rata@bcg.com>
2020-11-08 18:58:22 +0100

    formatting

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import json
 
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -27,43 +28,50 @@ def compute_fee():
         postal_code = form.postal_code.data
         price = str(int(form.price.data))
 
-        return redirect(url_for("list_agencies",postal_code=postal_code,price=price))
+        return redirect(url_for("list_agencies", postal_code=postal_code, price=price))
     return render_template(
         "compute_fee.html", title="Calcul le frais d'agence", form=form
     )
 
 
-@app.route("/list_agencies/<postal_code>/<price>", methods=['GET','POST'])
-def list_agencies(postal_code,price):
-    price_keuros =int(price)/ 1e3
+@app.route("/list_agencies/<postal_code>/<price>", methods=["GET", "POST"])
+def list_agencies(postal_code, price):
+    price_keuros = int(price) / 1e3
     postal_code = int(postal_code)
     engine = create_engine("postgresql://postgres:Bohr7411@localhost:5432/immoguru")
     agency_fees = pd.read_sql_query('select * from "agency_fees"', con=engine)
     agency_fees["price_max_keuros"] = agency_fees["price_max_keuros"].fillna(1e9)
-    agency_fees_relevant = agency_fees[
+    agency_fees_relevant =agency_fees[
         (agency_fees.postal_code == postal_code)
         & (agency_fees.price_min_keuros <= price_keuros)
         & (agency_fees.price_max_keuros > price_keuros)
-        ]
+    ]
     agency_fees_relevant["agency_fee_keuros"] = (
-            price_keuros * agency_fees_relevant["agency_rate"]
+        price_keuros * agency_fees_relevant["agency_rate"]
     )
     agency_fees_relevant["agency_fee_keuros"] = agency_fees_relevant[
         "agency_fee_keuros"
     ].fillna(agency_fees_relevant["agency_fee_min_keuros"])
     agency_fees_relevant["agency_fee_final"] = (
-            agency_fees_relevant["agency_fee_keuros"] * 1000
+        agency_fees_relevant["agency_fee_keuros"] * 1000
     )
     agency_fees_list = agency_fees_relevant[
-        ["agency_code", "agency_name", "agency_address", "postal_code", "agency_fee_final"]]
+        [
+            "agency_code",
+            "agency_name",
+            "agency_address",
+            "postal_code",
+            "agency_fee_final",
+        ]
+    ]
     agency_fees_list.sort_values(by="agency_fee_final", inplace=True)
-    agency_fees_list["agency_fee_final"] = [round(x) for x in agency_fees_list["agency_fee_final"]]
+    agency_fees_list["agency_fee_final"] = [
+        round(x) for x in agency_fees_list["agency_fee_final"]
+    ]
     agency_fees_list = agency_fees_list.set_index("agency_code")
     agency_fees_dict = agency_fees_list.to_dict("index")
-    #agency_fees_dict = json.dumps(agency_fees_dict)
+    # agency_fees_dict = json.dumps(agency_fees_dict)
     agencies = [x for x in agency_fees_dict.values()]
     return render_template(
-        "list_agencies.html",
-        title="Agency fees",
-        agencies=agencies,
+        "list_agencies.html", title="Agency fees", agencies=agencies,
     )
