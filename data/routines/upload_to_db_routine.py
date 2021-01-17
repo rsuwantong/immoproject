cb170f7637f82d60f67bb69f2803e22653efcbac
suwantong rata <suwantong.rata@bcg.com>
2021-01-17 18:34:22 +0100

    formatting + input file name change

--- data/routines/upload_to_db_routine.py
+++ data/routines/upload_to_db_routine.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 import psycopg2
 from data.data import Data
 
+
 class EtlUploadDB(Routine):
     """
     Class to proceed to upload ETL to the database
@@ -14,16 +15,39 @@ class EtlUploadDB(Routine):
 
     def run_routine(self):
         data = Data(self._config, use_cache=True)
-        engine = create_engine('postgresql://postgres:Bohr7411@localhost:5432/immoguru')
+        engine = create_engine("postgresql://postgres:Bohr7411@localhost:5432/immoguru")
         agency_fees = data.get("for_filling_agency_fees")
         agency_fees = agency_fees[~agency_fees.price_min.isna()]
         # TODO: Proper log when duplicates
         agency_fees = agency_fees.drop_duplicates()
-        agency_fees = agency_fees[["agency_code", "agency_name", "agency_address", "postal_code", "city", "price_min",
-                                   "agency_rate", "agency_fee_min_keuros"]]
+        agency_fees = agency_fees[
+            [
+                "agency_code",
+                "agency_name",
+                "agency_address",
+                "postal_code",
+                "city",
+                "price_min",
+                "agency_rate",
+                "agency_fee_min_keuros",
+            ]
+        ]
         agency_fees.rename(columns={"price_min": "price_min_keuros"}, inplace=True)
-        agency_fees['price_max_keuros'] = agency_fees.groupby(['agency_code'])['price_min_keuros'].transform(lambda x: x.shift(-1))
-        agency_fees = agency_fees[["agency_code", "agency_name", "agency_address", "postal_code", "city", "price_min_keuros", "price_max_keuros",
-                                   "agency_rate", "agency_fee_min_keuros"]]
+        agency_fees["price_max_keuros"] = agency_fees.groupby(["agency_code"])[
+            "price_min_keuros"
+        ].transform(lambda x: x.shift(-1))
+        agency_fees = agency_fees[
+            [
+                "agency_code",
+                "agency_name",
+                "agency_address",
+                "postal_code",
+                "city",
+                "price_min_keuros",
+                "price_max_keuros",
+                "agency_rate",
+                "agency_fee_min_keuros",
+            ]
+        ]
         # TODO: remove index
         agency_fees.to_sql("agency_fees", engine)
